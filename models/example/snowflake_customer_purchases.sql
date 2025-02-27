b7b6ed3cf12e6ee3f05257307035a4713073746d
Mohammad Abid Umer <mohammadabid.umer@perficient.com>
2025-02-27 18:02:57 +0530

    Added functionality for Sources

--- models/example/snowflake_customer_purchases.sql
+++ models/example/snowflake_customer_purchases.sql
@@ -3,8 +3,8 @@
 
 select c.c_custkey,c.c_name,c.c_nationkey as nation,
 sum(o.o_totalprice) as total_order_price
-from snowflake_sample_data.tpch_sf1.customer c
-left join snowflake_sample_data.tpch_sf1.orders o
+from {{ source('sample', 'customer')}} c
+left join {{ source('sample', 'orders')}} o
 on c.c_custkey = o.o_custkey
 group by c.c_custkey,c.c_name,c.c_nationkey
 having sum(o.o_totalprice) > 0
